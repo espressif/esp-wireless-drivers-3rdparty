d03f81925b3b02ee048d9eb18fdb5de6488ebdd3
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2023-05-25 10:40:57 -0300

    Init commit: add a script to get ESP-IDF's components
    Highly inspired by esp-hal-components, get Wi-Fi-related components
    from ESP-IDF and sync then on sync-branches into this repository.

---
+++ README.md
@@ -0,0 +1,38 @@
+# esp-wireless-drivers-3rdparty
+
+Wireless components for Espressif chips.
+
+Currently, the branches are all synchronized from IDF by scripts under given rules.
+
+Except for the scripts, no manual development will happen here.
+
+## Branch naming convention
+
+sync-[name]-[branch]
+
+Where:
+
+- `sync` means this is a branch maintained by the script. Will not be able to be merged with each other.
+- `[name]`: either a number (whose file list will be described somewhere), or a single component (in case someone needs it...)
+- `[branch]`: the IDF branch to sync from, e.g. `master`, `release/v5.0`
+
+## Existing branches
+
+- [`test-sync-1-release_v5.0`](../../tree/test-sync-1-release_v5.0):
+    - Based on IDF `release/v5.0` branch.
+    - Includes components: `esp_event`, `esp_phy`, `esp_wifi`, `mbedtls`, `wpa_supplicant`.
+## Restrictions
+
+1. Sync branches don't have common ancestors
+
+   This may cause some problems when you merge or pick from these branches
+
+2. Can't easily modify the file list of an existing branch
+
+   The tool used by the sync script converts commits of IDF into new ones on the given branch. The history is kept while the files are filtered according to the given file list.
+
+   The generated commits will have the same SHA as long as the commit author, date, message and change list are the same.
+
+   Any modification to the script's strategy of a sync branch (including modifying the file list) will change the SHA of the commits, forbidding the generated branch being merged (pushed) to the existing one.
+
+   When we need to modify the file list or any other part of the commit, it's suggested to create a new branch.
---
+++ tools/extract_idf_components.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+set -eux
+
+# ESP_WIRELESS_URL
+# IDF_URL
+
+# Usage: clone_idf ESP_IDF_BRANCH
+clone_idf() {
+    rm -rf download_idf
+    git clone --single-branch --branch "$1" "${IDF_URL}" download_idf
+}
+
+# Usage: find_pattern STRING
+find_pattern() {
+    git --no-pager log -i --grep "$1" > issue_found.txt
+    if [[ $(cat issue_found.txt | wc -l) > 0 ]]; then
+        ISSUE_NUM=$(grep "^commit " issue_found.txt | wc -l)
+        die "${ISSUE_NUM} $1 found. See issue_found.txt"
+    fi
+}
+
+check_links() {
+    find_pattern 'github.com/[^ /]*/[^ /]*/\(issues\|pull\)'
+    find_pattern 'espressif/esp-idf[!#$&~%^]'
+}
+
+# Usage: extract_components ESP_IDF_BRANCH SYNC_BRANCH_NAME ARGS...
+extract_components() {
+    ESP_IDF_BRANCH=$1
+    SYNC_BRANCH_NAME=$2
+    ARGS="${@:3}"
+
+    echo "Cloning ESP-IDF (${ESP_IDF_BRANCH})"
+
+    clone_idf "${ESP_IDF_BRANCH}"
+
+    echo "Extract to branch ${SYNC_BRANCH_NAME} with arg list: '$ARGS'"
+
+    FOLDER_NAME="esp-idf-${SYNC_BRANCH_NAME}"
+
+    rm -rf ${FOLDER_NAME}
+    cp -r download_idf ${FOLDER_NAME}
+
+    pushd ${FOLDER_NAME}
+    git filter-repo "${@:3}"
+
+    check_links
+
+    git checkout -B ${SYNC_BRANCH_NAME}
+    git push ${ESP_WIRELESS_URL} ${SYNC_BRANCH_NAME}
+    git clean -xdff
+    popd
+}
+
+# Usage get_arg_by_components [COMPONENTS...]
+get_arg_by_components() {
+    RET=""
+    for COMPONENT in $*
+    do
+        # There will be an redundant trailing space
+        RET+="--path components/${COMPONENT} "
+    done
+    echo ${RET}
+}
+
+LIC_ARG="--path LICENSE"
+
+# The commits have the same SHA as long as the commit author, date, message and change list are the same.
+# Any modification to the strategy will create new branch that cannot be merged (pushed) to the existing one.
+
+MSG_CALLBACK="
+        # (https)?github.com/[user]/[proj]/*/[id] -> [Github: [user]/proj]/*/[id]
+        msg1 = re.sub(br'(?:(?:https?://)?github\.com/)([^\s/]+/[^\s/]+)/([^/]+/[0-9]+)', br'[Github: \1]/\2', message)
+
+        # espressif/esp-idf -> [ESP_IDF] (except from [Github: espressif/esp-idf])
+        return re.sub(br'espressif/esp-idf([^\]])', br'[ESP_IDF]\1', msg1)
+    "
+ARG="${LIC_ARG} $(get_arg_by_components esp_event esp_phy esp_wifi mbedtls wpa_supplicant)"
+# extract_components "release/v5.0" "sync-1-release_v5.0" ${ARG} --message-callback "${MSG_CALLBACK}"
+
+# Add new one here if you have new requirement
+
+# Push to protected branch will cause that branch to appear on Github.
+# Try with non-protected branch first.
+# ARG="${LIC_ARG} $(get_arg_by_components esp_event esp_phy esp_wifi mbedtls wpa_supplicant)"
+extract_components "release/v5.0" "test-sync-1-release_v5.0" ${ARG} --message-callback "${MSG_CALLBACK}"
+
+############## Deprecated Syncs ###################
